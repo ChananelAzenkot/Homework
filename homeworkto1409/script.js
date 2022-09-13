8a3b1a77d95c5cea985f54e3f8077650148c6183
chananelaze <hananel600@gmail.com>
2022-09-13 21:52:27 +0300

    homeworkto1409

---
+++ homeworkto1409/script.js
@@ -0,0 +1,15 @@
+let mygame = [];
+let mygamerow = [];
+for (let x = 0; x < 8; x++) {
+	for (let y = 0; y < 8; y++) {
+		if ((x + y) % 2 == 0) {
+			mygamerow.push('black');
+		} else {
+			mygamerow.push('white');
+		}
+	}
+	mygame.push(mygamerow);
+	mygamerow = [];
+}
+
+console.table(mygame);
